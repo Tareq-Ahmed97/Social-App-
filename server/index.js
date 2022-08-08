5771deef31e2ebb651a239ada1688ab9a03c75c1
Tareq-Ahmed97 <107646016+Tareq-Ahmed97@users.noreply.github.com>
2022-08-08 16:23:49 +0600

    initiate server

--- server/index.js
+++ server/index.js
@@ -3,21 +3,25 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30 mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://TareqAhmed:peterparker9725@cluster0.aup84.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://tareq9725:peterparker9725@socialapp.4oyc69f.mongodb.net/?retryWrites=true&w=majority";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
   .then(() =>
-    app.listen(PORT, () => console.log("Server running on port:${PORT}"))
+    app.listen(PORT, () => console.log("Server running on port: ${PORT}"))
   )
   .catch((error) => console.log(error.message));
